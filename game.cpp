12f18332e6e8545e82463d69ed1b41100c33a916
rikutoiri <83803966+rikutoiri@users.noreply.github.com>
2021-07-13 14:27:41 +0900

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,40 @@
+#include "Game.h"
+#include "SceneMgr.h"
+#include "DxLib.h"
+#include "player.h"
+#include "Enemy.h"
+
+static int mImageHandle; //画像ハンドル格納用変数
+//初期化
+void Game_Initialize() {
+	mImageHandle = LoadGraph("images/haikeigazo.png"); //画像のロード
+
+	Player_Initialize();	//
+	Enemy_Initialize();
+}
+
+//終了処理
+void Game_Finalize() {
+	DeleteGraph(mImageHandle); //画像の解放
+
+	Player_Finalize();	//プレイヤーの開放
+	Enemy_Finalize();
+}
+
+//更新
+void Game_Update() {
+	if (CheckHitKey(KEY_INPUT_ESCAPE) != 0) { //Escキーが押されていたら
+		SceneMgr_ChangeScene(eScene_Menu);//シーンをメニューに変更
+	}
+	DrawGraph(0, 0, mImageHandle, FALSE);
+
+	Player_Update();
+	Enemy_Update();
+
+}
+//描画
+void Game_Draw() {
+	Player_Draw();
+	Enemy_Draw();
+
+}
